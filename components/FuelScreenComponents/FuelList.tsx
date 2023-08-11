73ab502c37c2e6479e1288f0c53149c8dc1045a5
Aleksandr <aleksiz1977@gmail.com>
2023-08-11 21:54:15 +0300

    finish fuelRow component

--- components/FuelScreenComponents/FuelList.tsx
+++ components/FuelScreenComponents/FuelList.tsx
@@ -1,12 +1,9 @@
-import { FlatList, ListRenderItem, StyleSheet, TouchableHighlight, View } from 'react-native'
+import { FlatList } from 'react-native'
 import { StateFuel } from '../../type'
-import { Button, ListItem } from '@rneui/themed'
 import { useEffect, useState } from 'react'
-import { useAppDispatch, useAppSelector } from '../Redux/hook'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import { delFuel } from '../Redux/actions'
-import { Surface, useTheme } from 'react-native-paper'
+import { useAppSelector } from '../Redux/hook'
 import { BusyIndicator } from '../useIsReadyHook'
+import { RenderRowFuel } from './fuelRow'
 
 interface handleProp {
   handlePress: (item: StateFuel) => void
@@ -15,97 +12,8 @@ interface handleProp {
 
 export const FuelList = ({ handlePress, filterList = 'last' }: handleProp): JSX.Element => {
   const listFuel = useAppSelector(state => state.cars[0].fuel)
-  const carId = useAppSelector(state => state.numberCar)
-  const dispatch = useAppDispatch()
-  const { colors } = useTheme()
   const [isSortFuel, setIsSortFuel] = useState(false)
   const [isLoad, setIsLoad] = useState(true)
-
-  const renderRow: ListRenderItem<StateFuel> = ({ item }: { item: StateFuel }) => {
-    return (
-
-      <View style={styles.listItem}>
-         <Surface elevation={1} >
-
-        <ListItem.Swipeable
-          animation={{ type: 'spring' }}
-          containerStyle={{ padding: 5, height: 70, backgroundColor: colors.surface }}
-
-          /* bottomDivider
-          topDivider */
-          leftContent={() => (
-            <Button
-              title='info'
-              icon={{
-                name: 'info',
-                color: 'white'
-              }}
-              buttonStyle={{ minHeight: '100%' }}
-              onPress={() => {
-                handlePress(item)
-              }}
-              /* onPress={() => { nav() }} */
-            />
-          )}
-          rightContent={() => (
-            <Button
-              title='delete'
-              icon={{
-                name: 'delete',
-                color: 'white'
-              }}
-              buttonStyle={{
-                minHeight: '100%',
-                backgroundColor: 'red'
-              }}
-              onPress={() => {
-                dispatch(delFuel(carId, item.id))
-              }}
-            />
-          )}
-          Component={TouchableHighlight}
-
-          onPress={() => handlePress(item)}
-        >
-
-          <MaterialCommunityIcons name={'gas-station'} size={24} color={colors.tertiary}/>
-
-          <ListItem.Content style={{
-            flex: 1.7
-          }}>
-            <ListItem.Title style={{ fontSize: 14, color: colors.onSurface }}>
-              {String(new Date(item.dateFuel).toLocaleDateString())}
-            </ListItem.Title>
-            <ListItem.Subtitle style={{ fontSize: 14, color: colors.onSurface }}>
-              {item.StationFuel}
-            </ListItem.Subtitle>
-          </ListItem.Content>
-
-          <ListItem.Content style={{ flex: 1.5 }}>
-            <ListItem.Title style={{ fontSize: 14, color: colors.onSurface }}>
-              {item.typeFuel}
-            </ListItem.Title>
-            <ListItem.Subtitle style={{ fontSize: 14, color: colors.onSurface }}>
-              {item.CostFuel} грн/л
-            </ListItem.Subtitle>
-          </ListItem.Content>
-          <ListItem.Content>
-            <ListItem.Title style={{ fontSize: 14, color: colors.onSurface }}>
-              {item.volumeFuel} л
-            </ListItem.Title>
-          </ListItem.Content>
-          <ListItem.Content>
-            <ListItem.Title style={{ fontSize: 14, color: colors.onSurface }}>
-              {item.AmountFuel} грн
-            </ListItem.Title>
-          </ListItem.Content>
-        </ListItem.Swipeable>
-
-        </Surface>
-      </View>
-
-    )
-  }
 
   useEffect(() => {
     listFuel.sort(function (a, b) {
@@ -137,8 +45,7 @@ export const FuelList = ({ handlePress, filterList = 'last' }: handleProp): JSX.
     scrollEnabled
     data={filter()}
     extraData={isSortFuel}
-    renderItem={(item) => renderRow(item)}
-    /* estimatedItemSize={70} */
+    renderItem={({ item }) => <RenderRowFuel item={item} handlePress={handlePress} />}
     keyExtractor={(item, index) => index.toString()}
     getItemLayout={(data, index) => (
       {
@@ -147,30 +54,9 @@ export const FuelList = ({ handlePress, filterList = 'last' }: handleProp): JSX.
         index
       }
     )}
-    initialNumToRender={5}
+    initialNumToRender={6}
   />
 }
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  listItem: {
-    height: 70,
-    paddingRight: 0,
-    marginHorizontal: 5,
-    marginVertical: 5,
-    flex: 1
-    /* backgroundColor: 'red' */
-    /* borderRadius: 5 */
-    /* shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1
-    },
-    shadowOpacity: 0.20,
-    shadowRadius: 1.41,
-
-    elevation: 2 */
-  }
-})
---
+++ components/FuelScreenComponents/fuelRow.tsx
@@ -0,0 +1,150 @@
+import { StyleSheet, View } from 'react-native'
+import { StateFuel } from '../../type'
+import { Card, IconButton, Menu, useTheme } from 'react-native-paper'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useState } from 'react'
+import { delFuel } from '../Redux/actions'
+import { useAppDispatch, useAppSelector } from '../Redux/hook'
+
+interface propsRowFuel {
+  handlePress: (item: StateFuel) => void
+  item: StateFuel
+}
+
+export const RenderRowFuel = ({ item, handlePress }: propsRowFuel): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const carId = useAppSelector(state => state.numberCar)
+
+  const [visibleMenu, setVisibleMenu] = useState(false)
+  const { colors } = useTheme()
+
+  const openMenu = (): void => {
+    setVisibleMenu(true)
+  }
+  const closeMenu = (): void => {
+    setVisibleMenu(false)
+  }
+
+  return (
+
+    <View style={styles.listItem}>
+
+      <Card
+        style={{ height: 70 }}
+        contentStyle={{ flexDirection: 'row' }}
+        /* bottomDivider
+        topDivider */
+        /* leftContent={() => (
+          <Button
+            title='info'
+            icon={{
+              name: 'info',
+              color: 'white'
+            }}
+            buttonStyle={{ minHeight: '100%' }}
+            onPress={() => {
+              handlePress(item)
+            }}
+            /!* onPress={() => { nav() }} *!/
+          />
+        )} */
+        /* rightContent={() => (
+          <Button
+            title='delete'
+            icon={{
+              name: 'delete',
+              color: 'white'
+            }}
+            buttonStyle={{
+              minHeight: '100%',
+              backgroundColor: 'red'
+            }}
+            onPress={() => {
+              dispatch(delFuel(carId, item.id))
+            }}
+          />
+        )} */
+
+        onPress={() => handlePress(item)}
+      >
+
+        <Card.Title
+          style={{ flex: 3.6, paddingLeft: 12 }}
+          leftStyle={{ marginRight: 2 }}
+          left={() =>
+            <MaterialCommunityIcons name={'gas-station'} size={24} color={colors.tertiary} style={{}}/>
+          }
+          title={String(new Date(item.dateFuel).toLocaleDateString('ru', { day: '2-digit', month: '2-digit', year: '2-digit' }))}
+          titleStyle={{ paddingRight: 2 }}
+          subtitleStyle={{ paddingRight: 2 }}
+          subtitle={item.StationFuel}
+          titleVariant={'bodyMedium'}
+          subtitleVariant={'bodyMedium'}
+        />
+        {/* <ListItem.Title style={{ fontSize: 14, color: colors.onSurface }}>
+              {String(new Date(item.dateFuel).toLocaleDateString())}
+            </ListItem.Title> */}
+        {/* <ListItem.Subtitle style={{ fontSize: 14, color: colors.onSurface }}>
+              {item.StationFuel}
+            </ListItem.Subtitle> */}
+
+        <Card.Title
+          style={{ flex: 2.3 }}
+
+          title={String(item.volumeFuel) + ' л'}
+          subtitle = {String(item.CostFuel) + ' грн/л'}
+          titleVariant={'bodyMedium'}
+          subtitleVariant={'bodyMedium'}
+          titleStyle={{ paddingRight: 2 }}
+          subtitleStyle={{ paddingRight: 2 }}
+        />
+
+        <Card.Title
+          style={{ flex: 2.2 }}
+          title={String(item.AmountFuel) + ' грн'}
+          titleVariant={'bodyMedium'}
+          subtitleVariant={'bodyMedium'}
+          titleStyle={{ paddingRight: 2 }}
+          subtitleStyle={{ paddingRight: 2 }}
+        />
+        <View style={{ justifyContent: 'flex-start' }}>
+          <Menu
+
+            anchor={
+              <IconButton icon={'dots-vertical'} size={24} style={{ margin: 2, alignItems: 'flex-end' }} onPress={openMenu}/>
+            }
+            visible={visibleMenu}
+            onDismiss={closeMenu}
+          >
+            <Menu.Item title={'delete'}
+                       dense
+                       leadingIcon={'delete'}
+                       onPress={() => {
+                         dispatch(delFuel(carId, item.id))
+                         closeMenu()
+                       }}
+            />
+            <Menu.Item title={'edit'}
+                       onPress={() => {
+                         handlePress(item)
+                       }}
+                       dense
+                       leadingIcon={'file-document-edit'}
+            />
+          </Menu>
+        </View>
+      </Card>
+
+    </View>
+
+  )
+}
+const styles = StyleSheet.create({
+  listItem: {
+    height: 70,
+    paddingRight: 0,
+    marginHorizontal: 5,
+    marginVertical: 5,
+    flex: 1
+  }
+})
