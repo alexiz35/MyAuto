35680c3b7b7ecdab67c906628c354347747e8983
Aleksandr <aleksiz1977@gmail.com>
2024-02-12 14:53:14 +0200

    created ControlCard

---
+++ components/SettingScreenComponents/ControlCard.tsx
@@ -0,0 +1,88 @@
+import { Card, Checkbox, Divider, Icon, Text } from 'react-native-paper'
+import { View } from 'react-native'
+import { useAppDispatch, useAppSelector } from '../Redux/hook'
+import { useState } from 'react'
+import { changeAlarmStart } from '../Redux/SettingSlice'
+import { changeAlarmPeriod, changeAlarmPeriodNumber } from '../Redux/actions'
+import { useAppTheme } from '../../CommonComponents/Theme'
+import { stylesSettingScreen } from '../../screens/SettingScreen'
+
+export const ControlCard = () => {
+  const dispatch = useAppDispatch()
+  const state = useAppSelector((state) => state)
+  const { colors } = useAppTheme()
+
+  // -----------------------------------------------------------------------------
+  // ****************************** ALARM section *********************************
+
+  const [checkAlarmStart, setCheckAlarmStart] = useState<
+  'checked' | 'unchecked' | 'indeterminate'
+  >(state.setting.alarmMileageStart ? 'checked' : 'unchecked')
+  const [checkAlarmPeriod, setCheckAlarmPeriod] = useState<
+  'checked' | 'unchecked' | 'indeterminate'
+  >(state.setting.alarmMileagePeriod ? 'checked' : 'unchecked')
+  const [checkAlarmPeriodNumber, setCheckAlarmPeriodNumber] = useState<
+  'checked' | 'unchecked' | 'indeterminate'
+  >(state.setting.alarmMileagePeriodNumber === 2 ? 'unchecked' : 'checked')
+  const pressAlarm = (typeCheck: string): void => {
+    switch (typeCheck) {
+      case 'alarmStart':
+        if (checkAlarmStart === 'checked') {
+          setCheckAlarmStart('unchecked')
+          dispatch(changeAlarmStart(false))
+        } else if (checkAlarmStart === 'unchecked') {
+          setCheckAlarmStart('checked')
+          dispatch(changeAlarmStart(true))
+        }
+        break
+      case 'alarmPeriod':
+        if (checkAlarmPeriod === 'checked') {
+          setCheckAlarmPeriod('unchecked')
+          dispatch(changeAlarmPeriod(false))
+        } else if (checkAlarmPeriod === 'unchecked') {
+          setCheckAlarmPeriod('checked')
+          dispatch(changeAlarmPeriod(true))
+        }
+        break
+      case 'alarmPeriodNumber':
+        if (checkAlarmPeriodNumber === 'checked') {
+          setCheckAlarmPeriodNumber('unchecked')
+          dispatch(changeAlarmPeriodNumber(2))
+        } else if (checkAlarmPeriodNumber === 'unchecked') {
+          setCheckAlarmPeriodNumber('checked')
+          dispatch(changeAlarmPeriodNumber(1))
+        }
+        break
+    }
+  }
+
+  return (
+    <Card style={{ marginVertical: 5 }}>
+      <View style={stylesSettingScreen.iconText}>
+        <Icon source={'circle'} color={colors.tertiary} size={10} />
+        <Text style={stylesSettingScreen.text}>Контроль пробега</Text>
+      </View>
+      <Checkbox.Item
+        status={checkAlarmStart}
+        label={'Напоминание при входе в приложении'}
+        onPress={() => { pressAlarm('alarmStart') }}
+        labelVariant={'bodyMedium'}
+      />
+      <Divider horizontalInset />
+      <Checkbox.Item
+        status={checkAlarmPeriod}
+        label={'Периодическое напоминание в фоне'}
+        onPress={() => { pressAlarm('alarmPeriod') }}
+        labelVariant={'bodyMedium'}
+      />
+      <Divider horizontalInset />
+      <Checkbox.Item
+        status={checkAlarmPeriodNumber}
+        label={'Синхронизация пробега с авто'}
+        onPress={() => { pressAlarm('alarmPeriodNumber') }}
+        labelVariant={'bodyMedium'}
+      />
+    </Card>
+
+  )
+}
--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -44,74 +44,16 @@ import { GoogleCard } from '../components/SettingScreenComponents/GoogleCard'
 import { ThemeCard } from '../components/SettingScreenComponents/ThemeCard'
 import { CarsCard } from '../components/SettingScreenComponents/CarsCard'
 import { SellersCard } from '../components/SettingScreenComponents/SellersCard'
+import { ControlCard } from '../components/SettingScreenComponents/ControlCard'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'SettingScreen'>
 
 // -------------------------------------------------------------------------------------------
 
-export const initialCarState = {
-  info: initialStateInfo, // ok
-  carId: 0,
-  currentMiles: {
-    currentMileage: 0,
-    dateMileage: new Date()
-  }, // ok
-  fuel: [], // ok
-  services: [],
-  mileage: [], // ok
-  parts: [], // ok
-  others: [], // ok
-  tasks: [] // ok
-}
-
 const SettingScreen = ({ navigation }: Props): JSX.Element => {
   const state = useAppSelector((state) => state)
   const dispatch = useAppDispatch()
   const { colors } = useAppTheme()
-
-  // -----------------------------------------------------------------------------
-  // ****************************** ALARM section *********************************
-
-  const [checkAlarmStart, setCheckAlarmStart] = useState<
-  'checked' | 'unchecked' | 'indeterminate'
-  >(state.setting.alarmMileageStart ? 'checked' : 'unchecked')
-  const [checkAlarmPeriod, setCheckAlarmPeriod] = useState<
-  'checked' | 'unchecked' | 'indeterminate'
-  >(state.setting.alarmMileagePeriod ? 'checked' : 'unchecked')
-  const [checkAlarmPeriodNumber, setCheckAlarmPeriodNumber] = useState<
-  'checked' | 'unchecked' | 'indeterminate'
-  >(state.setting.alarmMileagePeriodNumber === 2 ? 'unchecked' : 'checked')
-  const pressAlarm = (typeCheck: string): void => {
-    switch (typeCheck) {
-      case 'alarmStart':
-        if (checkAlarmStart === 'checked') {
-          setCheckAlarmStart('unchecked')
-          dispatch(changeAlarmStart(false))
-        } else if (checkAlarmStart === 'unchecked') {
-          setCheckAlarmStart('checked')
-          dispatch(changeAlarmStart(true))
-        }
-        break
-      case 'alarmPeriod':
-        if (checkAlarmPeriod === 'checked') {
-          setCheckAlarmPeriod('unchecked')
-          dispatch(changeAlarmPeriod(false))
-        } else if (checkAlarmPeriod === 'unchecked') {
-          setCheckAlarmPeriod('checked')
-          dispatch(changeAlarmPeriod(true))
-        }
-        break
-      case 'alarmPeriodNumber':
-        if (checkAlarmPeriodNumber === 'checked') {
-          setCheckAlarmPeriodNumber('unchecked')
-          dispatch(changeAlarmPeriodNumber(2))
-        } else if (checkAlarmPeriodNumber === 'unchecked') {
-          setCheckAlarmPeriodNumber('checked')
-          dispatch(changeAlarmPeriodNumber(1))
-        }
-        break
-    }
-  }
 
   return (
     <BackgroundView>
@@ -130,7 +72,7 @@ const SettingScreen = ({ navigation }: Props): JSX.Element => {
         {/*
          ************************** Car SWITCH  ******************************************
          */}
-              <CarsCard/>
+           <CarsCard/>
         {/*
          ************************** GOOGLE BACKUP  ******************************************
          */}
@@ -138,33 +80,7 @@ const SettingScreen = ({ navigation }: Props): JSX.Element => {
         {/*
          ************************** ALARM BLOCK  ******************************************
          */}
-        <Card style={{ marginVertical: 5 }}>
-          <View style={styles.iconText}>
-            <Icon source={'circle'} color={colors.tertiary} size={10} />
-            <Text style={styles.text}>Контроль пробега</Text>
-          </View>
-          <Checkbox.Item
-            status={checkAlarmStart}
-            label={'Напоминание при входе в приложении'}
-            onPress={() => { pressAlarm('alarmStart') }}
-            labelVariant={'bodyMedium'}
-          />
-          <Divider horizontalInset />
-          <Checkbox.Item
-            status={checkAlarmPeriod}
-            label={'Периодическое напоминание в фоне'}
-            onPress={() => { pressAlarm('alarmPeriod') }}
-            labelVariant={'bodyMedium'}
-          />
-          <Divider horizontalInset />
-          <Checkbox.Item
-            status={checkAlarmPeriodNumber}
-            label={'Синхронизация пробега с авто'}
-            onPress={() => { pressAlarm('alarmPeriodNumber') }}
-            labelVariant={'bodyMedium'}
-          />
-        </Card>
-
+            <ControlCard/>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
         <TouchableHighlight
           onPress={async () => {
@@ -179,7 +95,7 @@ const SettingScreen = ({ navigation }: Props): JSX.Element => {
 }
 export default SettingScreen
 
-const styles = StyleSheet.create({
+export const stylesSettingScreen = StyleSheet.create({
   iconText: {
     flexDirection: 'row',
     padding: 10,
