37f4258b19cb33ac5ac96f7c7342a5620e2b8b4f
Aleksandr <aleksiz1977@gmail.com>
2024-04-08 16:30:05 +0300

    added typescript for i18n

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,27 +1,15 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-// eslint-disable-next-line import/no-unresolved
 import commonRu from './ru/common'
 import commonEn from './en/common'
-import naviEn from './en/navi'
-import naviRu from './ru/navi'
-import setRu from './ru/setting'
-import setEn from './en/setting'
-import * as constants from 'constants'
 const { languageDetectorPlugin } = require('./languageDetectorPlugin')
-/* import ru from './translation/ru.json'
-import en from './translation/en.json' */
 
 export const resources = { // list of languages
   ru: {
     translation: commonRu
-    /* navigation: naviRu,
-    setting: setRu */
   },
   en: {
     translation: commonEn
-    /* navigation: naviEn,
-    setting: setEn */
   }
 } as const
 
@@ -31,8 +19,6 @@ void i18n
   .init({
     compatibilityJSON: 'v3', // To make it work for Android devices, add this line.
     resources,
-    /* lng: 'en', */ // default language to use.
-    // if you're using a language detector, do not define the lng option
     fallbackLng: 'ru',
     interpolation: {
       escapeValue: false
