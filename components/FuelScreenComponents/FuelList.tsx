e03569972bbcd23cb8760c67f8a87daffef55dcc
Aleksandr <aleksiz1977@gmail.com>
2024-05-30 15:43:22 +0300

    refactoring sortFuel

--- components/FuelScreenComponents/FuelList.tsx
+++ components/FuelScreenComponents/FuelList.tsx
@@ -1,5 +1,5 @@
 import { FlatList } from 'react-native'
-import { getIndexCar, StateFuel } from '../../type'
+import { getIndexCar, StateFuel, StateOther } from '../../type'
 import { useEffect, useState } from 'react'
 import { useAppSelector } from '../Redux/hook'
 import { BusyIndicator } from '../useIsReadyHook'
@@ -12,27 +12,30 @@ interface handleProp {
 
 export const FuelList = ({ handlePress, filterList = 'last' }: handleProp): JSX.Element => {
   const listFuel = useAppSelector(state => state.cars[getIndexCar(state.cars, state.numberCar)].fuel)
-  const [isSortFuel, setIsSortFuel] = useState(false)
   const [isLoad, setIsLoad] = useState(true)
-
-  /* useEffect(() => {
-    listFuel.sort(function (a, b) {
-      // @ts-expect-error data
-      return Date.parse(b.dateFuel) - Date.parse(a.dateFuel)
-    })
-    setIsSortFuel(!isSortFuel)
-  }, [listFuel]) */
+  const [stateFuel, setStateFuel] = useState<StateFuel[]>(listFuel)
 
   useEffect(() => {
     setTimeout(() => { setIsLoad(false) }, 10)
     setIsLoad(true)
-  }, [filterList])
+    setStateFuel(filter())
+  }, [filterList, listFuel])
+
+  const sort = (array: StateFuel[]) => {
+    if (array.length > 1) {
+      return array.slice().sort(function (a, b) {
+        // @ts-expect-error date
+        return Date.parse(new Date(b.dateFuel)) - Date.parse(new Date(a.dateFuel))
+      })
+    }
+    return array
+  }
 
   const filter = (): StateFuel[] => {
     switch (filterList) {
-      case 'last': return listFuel.slice(0, 3)
-      case 'ten': return listFuel.slice(0, 10)
-      default: return listFuel
+      case 'last': return sort(listFuel).slice(0, 3)
+      case 'ten': return sort(listFuel).slice(0, 10)
+      default: return sort(listFuel)
     }
   }
 
@@ -42,8 +45,8 @@ export const FuelList = ({ handlePress, filterList = 'last' }: handleProp): JSX.
       ? <BusyIndicator/>/* <ActivityIndicator size={'large'} color={'green'} /> */
       : <FlatList
     scrollEnabled
-    data={filter()}
-    extraData={isSortFuel}
+    data={stateFuel}
+    /* extraData={isSortFuel} */
     renderItem={({ item }) => <RenderRowFuel item={item} handlePress={handlePress} />}
     keyExtractor={(item, index) => index.toString()}
     getItemLayout={(data, index) => (
